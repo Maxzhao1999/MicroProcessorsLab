f3fc39ff504cb593946b97871e6527fbbfa8d789
maxzhao1999 <maxzhao@hotmail.co.uk>
2019-10-07 13:50:04 +0100

    Press button to change count!

--- Simple1.asm
+++ Simple1.asm
@@ -9,11 +9,15 @@
 start
 	movlw 	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
+	movlw	0xFF
+	movwf	TRISD, ACCESS
+	movlw	0x0
 	bra 	test
 loop	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
 	movlw 	0x63
+	ADDWF	PORTD, 0, ACCESS
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
