302d9c958b669d96e030ded5f5369397017855a9
maxzhao1999 <maxzhao@hotmail.co.uk>
2019-10-07 16:52:21 +0100

    Read, write and compare the FSRs

--- Read_write.asm
+++ Read_write.asm
@@ -1,4 +1,4 @@
-
+	#include p18f87k22.inc
 	code
 	org 0x0
 	goto	start
@@ -7,18 +7,43 @@
 
 start
 	movlw	0x10		    ; Set epoch
-	movwf	0x06, Access	    ; Store in data memory
+	movwf	0x06, ACCESS	    ; Store in data memory
+	movwf	0x07, ACCESS
+	movwf	0x08, ACCESS
 	lfsr	FSR0, 0x020
 	movlw	0x0
 	call	write
-	end
+	movlw	0x10
+	lfsr	FSR1, 0x040
+	call	read
+	movlw	0x0
+	movwf	TRISE, ACCESS
+	call compare
+	goto 0x0
 
 write
-	movwf	POSTINC0, f
+	movwf	POSTINC0
 	addlw	0x1
-	decfsz	0x06, Access
+	decfsz	0x06, ACCESS
 	bra write
 	return
 	
+read
+	movff	POSTDEC0, W
+	movwf	POSTINC1
+	decfsz	0x07, ACCESS
+	bra	read
+	return
+	
+compare
+	movff	POSTDEC0, W
+	subwf	POSTINC1, 0
+	movwf	PORTE
+	decfsz	0x08, ACCESS
+	bra	compare
+	return
+	
+endProgram
+	end
 ;read
 	
