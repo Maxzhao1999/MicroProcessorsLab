068eda731e8c8265e9ff243d19d7c2be910832e3
maxzhao1999 <maxzhao@hotmail.co.uk>
2019-10-14 16:51:48 +0100

    External memory 2 byte working

--- ExternalMemory.asm
+++ ExternalMemory.asm
@@ -7,6 +7,11 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+;	clrf	PORTC
+;	clrf	LATE
+;	banksel	ANCON1
+;	movlw	00h
+;	movwf	ANCON1
 	movlw	0x0
 	movwf	TRISD		    ;set port D as output
 	movlw	0xFF
@@ -17,24 +22,31 @@ start
 	bsf	PADCFG1, REPU, BANKED	;port E pull-up enabled
 	movlb	0x00		    ;move 0x00 to BSR
 	movlw	0xFF
-	movwf   PORTD, ACCESS	    ;set OE1 OE2 to high, CP1 CP2 to low
+	movwf   PORTD, ACCESS	    ;set OE1 OE2 to high, CP1 CP2 to high
 	movwf	PORTE, ACCESS       ;ensure all port E high
-	movlw	0xFF
-	movwf	0x01, ACCESS
-	movwf	PORTC, ACCESS
-	call	controlwrite
-	call	controlread
-	movff	0x02, PORTH
-	clrf	TRISC
-	clrf	TRISH
+	movlw	0xFA
+	movwf	0x01, ACCESS	
+	call	controlwrite1
+	call	controlread1
+	movff	0x02, PORTC
+	
+	movlw	0x99
+	movwf	0x03, ACCESS
+	call	controlwrite2
+	call	controlread2
+	movff	0x04, PORTH
+	
+	movlw	0x0
+	movwf	TRISC
+	movwf	TRISH
 	
 	goto	0x0
 
 	
-controlwrite
+controlwrite1
 	movlw	0x0
 	movwf	TRISE		    ;set port E as output
-	movlw	0xFA
+	movlw	0xFE
 	movwf	PORTD, ACCESS	    ;set cp1 to low
 	movff	0x01, PORTE
 	call	delay
@@ -43,19 +55,41 @@ controlwrite
 	setf	TRISE
 	return
 	
-controlread
+controlread1
 	movlw	0xFF
 	movwf	TRISE		    ;set port E to input
-	movlw	0xF5
+	movlw	0xFD
 	movwf	PORTD, ACCESS	    ;set OE1 to low
 	call	delay
-	movff	PORTE, 0x02
+	movff	LATE, 0x02
 	movlw	0xFF
 	movwf	PORTD, ACCESS
 	setf	TRISE
 	return
 	
-
+controlwrite2
+	movlw	0x0
+	movwf	TRISE		    ;set port E as output
+	movlw	0xFB
+	movwf	PORTD, ACCESS	    ;set cp2 to low
+	movff	0x03, PORTE
+	call	delay
+	movlw	0xFF
+	movwf	PORTD, ACCESS
+	setf	TRISE
+	return
+	
+controlread2
+	movlw	0xFF
+	movwf	TRISE		    ;set port E to input
+	movlw	0xF7
+	movwf	PORTD, ACCESS	    ;set OE2 to low
+	call	delay
+	movff	LATE, 0x04
+	movlw	0xFF
+	movwf	PORTD, ACCESS
+	setf	TRISE
+	return
 
 
 	
