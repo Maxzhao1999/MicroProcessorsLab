904366bd6d567ca6c70aa57026a0af735af0a165
maxzhao1999 <maxzhao@hotmail.co.uk>
2019-10-21 11:51:00 +0100

    write to 2nd line

--- Simple1.asm
+++ Simple1.asm
@@ -1,30 +1,32 @@
 	#include p18f87k22.inc
 
 	extern	UART_Setup, UART_Transmit_Message  ; external UART subroutines
-	extern  LCD_Setup, LCD_Write_Message	    ; external LCD subroutines
+	extern  LCD_Setup, LCD_Write_Message, LCD_Clear, LCD_ln2	    ; external LCD subroutines
 	
 acs0	udata_acs   ; reserve data space in access ram
 counter	    res 1   ; reserve one byte for a counter variable
 delay_count res 1   ; reserve one byte for counter in the delay routine
 
 tables	udata	0x400    ; reserve data anywhere in RAM (here at 0x400)
-myArray res 0x80    ; reserve 128 bytes for message data
+myArray res	0x80    ; reserve 128 bytes for message data
 
 rst	code	0    ; reset vector
 	goto	setup
 
 pdata	code    ; a section of programme memory for storing data
 	; ******* myTable, data in programme memory, and its length *****
-myTable data	    "Hello World!\n"	; message, plus carriage return
-	constant    myTable_l=.13	; length of data
-	
+myTable data	    "omae wa mou shin"	; message, plus carriage return
+	constant    myTable_l=.17	; length of data
+
 main	code
 	; ******* Programme FLASH read Setup Code ***********************
 setup	bcf	EECON1, CFGS	; point to Flash program memory  
 	bsf	EECON1, EEPGD 	; access Flash program memory
 	call	UART_Setup	; setup UART
 	call	LCD_Setup	; setup LCD
+	call	LCD_ln2		; line 2
 	goto	start
+;	call	LCD_Clear
 	
 	; ******* Main programme ****************************************
 start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
