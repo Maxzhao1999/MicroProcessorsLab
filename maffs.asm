cc1a1ddb76bd6839584d8a3f433eff95913c51c0
maxzhao1999 <maxzhao@hotmail.co.uk>
2019-10-25 09:54:24 +0100

    implement 8 bit*24 bit

--- maffs.asm
+++ maffs.asm
@@ -16,7 +16,8 @@ re1_1	res 1
 re1_2	res 1
 re1_3	res 1
 
-m_8_16
+
+m_8_16		;ab*cdef
 	movff	ab, W
 	mulwf	ef, ACCESS
 	movff	PRODH, re0_1
@@ -29,7 +30,7 @@ m_8_16
 	addwfc	re0_2
 	return
 
-m_16_16	
+m_16_16		;ghab*cdef
 	call	m_8_16
 	movff	re0_0, re1_0
 	movff	re0_1, re1_1
@@ -43,13 +44,20 @@ m_16_16
 	movff	re0_1, W
 	addwf	re1_2, W
 	movlw	0x0
-	addwfc	re1_3
 	movff	re0_2, re1_3
+	addwfc	re1_3
 	return
 	
-m_8_24
-
-
-
-
-
+m_8_24		;ab*ijcdef
+	call	m_8_16
+	movff	re0_0, re1_0
+	movff	re0_1, re1_1
+	movff	re0_2, re1_2
+	movff	ab, W
+	mulwf	ij
+	movff	PRODL, W
+	addwf	re1_2
+	movff	PRODH, re1_3
+	movlw	0x0
+	addwfc	re1_3
+	return
