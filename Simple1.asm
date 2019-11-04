ffb927634b9cfc234bcb2093902602b1d929a871
maxzhao1999 <maxzhao@hotmail.co.uk>
2019-11-04 16:53:48 +0000

    timer incomplete

--- Simple1.asm
+++ Simple1.asm
@@ -1,12 +1,12 @@
 	#include p18f87k22.inc
 
 	extern	UART_Setup, UART_Transmit_Message   ; external UART subroutines
-	extern  LCD_Setup, LCD_Write_Message	    ; external LCD subroutines
+	extern  LCD_Setup, LCD_Write_Message,LCD_delay_ms	    ; external LCD subroutines
 	extern	LCD_Write_Hex			    ; external LCD subroutines
 	extern  ADC_Setup, ADC_Read		    ; external ADC routines
 	extern  convert_to_decimal, dec_0, dec_2, cpr1h, cpr1l, cpr2h, cpr2l, f_count, thresh, thresl, compare
-	extern	Timer_Setup, loopsh, loopsl
-	global	frequencyl, frequencyh
+	extern	Timer_Setup, loopsh, loopsl,fcounter
+	global	frequencyl, frequencyh, calc
 acs0	udata_acs   ; reserve data space in access ram
 counter	    res 1   ; reserve one byte for a counter variable
 delay_count res 1   ; reserve one byte for counter in the delay routine
@@ -61,42 +61,96 @@ start
 ;	call	UART_Transmit_Message
 	
 measure_loop
-	movlw	0x0B
-	movwf	thresh
-	movlw	0xFF
-	movwf	thresl
-	call	compare
-	movlw	0x7A
-	cpfsgt	loopsh
-	bra	measure_loop		; goto current line in code
-	goto	calc
+	bra	measure_loop
+;	movlw	0x0B
+;	movwf	thresh
+;	movlw	0xFF
+;	movwf	thresl
+;	call	compare
+;	movlw	0x7A
+;	cpfsgt	loopsh
+;	bra	measure_loop		; goto current line in code
+;	goto	calc
+;carry
+;	movlw	0x0
+;	addwfc	frequencyh, 1
+;	clrf	frequencyl
+;	return
+;	
+;calc
+;	movlw	0x02
+;	subwf	f_count
+;	movlw	0x01
+;	addwf	frequencyl
+;	bc	carry
+;	movlw	0x02
+;	cpfsgt	f_count
+;	goto	display
+;	goto	calc
+;display
+;	call	convert_to_decimal
+;	movlw	0xFF
+;	call	LCD_delay_ms
+;	movf	fcounter,W
+;	call	LCD_Write_Hex
+;	movf	fcounter,W
+;	call	LCD_Write_Hex
+;	calc
+calc
+	movlw	0x02
+	subwf	fcounter
+	movlw	0x01
+	addwf	frequencyl
+	bc	carry
+	movlw	0x02
+	cpfsgt	fcounter
+	bra	display
+	bra	calc
+display
+;	call	convert_to_decimal
+	
+	movf	fcounter, W, ACCESS
+	call	LCD_Write_Hex
+;	movf	dec_0, W, ACCESS
+;	call	LCD_Write_Hex
+;	call	delay
+	return
 carry
 	movlw	0x0
 	addwfc	frequencyh, 1
 	clrf	frequencyl
 	return
 	
-calc
-	movlw	0x02
-	subwf	f_count
-	movlw	0x01
-	addwf	frequencyl
-	bc	carry
-	movlw	0x02
-	cpfsgt	f_count
-	goto	display
-	goto	calc
-display
-	call	convert_to_decimal
-	movf	dec_2,W
-	call	LCD_Write_Hex
-	movf	dec_0,W
-	call	LCD_Write_Hex
-	bra	measure_loop
+	
+;	clrf	fcounter
+	
 
 	; a delay subroutine if you need one, times around loop in delay_count
-delay	decfsz	delay_count	; decrement until zero
-	bra delay
-	return
+;delay	decfsz	delay_count	; decrement until zero
+;	bra delay
+;	return
 
+delay
+	movlw	0x0F
+	movwf	0x09, ACCESS
+dloop	call	delayx
+	decfsz	0x09, f, ACCESS
+	bra	dloop
+	return	FAST
+	
+delayx
+	movlw	0xFF
+	movwf	0x10, ACCESS
+dloopx	call	delayy
+	decfsz	0x10, f, ACCESS
+	bra	dloopx
+	return
+	
+delayy
+	movlw	0xF
+	movwf	0x11, ACCESS
+dloopy
+	decfsz	0x11, f, ACCESS
+	bra	dloopy
+	return
 	end
