23df657a9d1f87dc7bd887e5d0048d58fb92ba4d
maxzhao1999 <maxzhao@hotmail.co.uk>
2019-11-04 16:53:39 +0000

    timer incomplete

--- Timer_interrupt.asm
+++ Timer_interrupt.asm
@@ -1,21 +1,24 @@
 #include p18f87k22.inc
 	
-	global	Timer_Setup, loopsh, loopsl
-
+	global	Timer_Setup, loopsh, loopsl,fcounter	
+	extern  calc, frequencyl, frequencyh
 acs0    udata_acs   ; named variables in access ram
 loopsl	res 1   ; reserve 1 byte for variable LCD_cnt_l
 loopsh	res 1
+fcounter    res 1
 	
 int_hi	code	0x0008	; high vector, no low vector
-	movf	loopsl, W, ACCESS
+	btfsc	INTCON,TMR0IF ; check that this is timer0 interrupt
+	bra	timer
+	movf	fcounter, W, ACCESS
 	addlw	0x01
-	movwf	loopsl, ACCESS
-	bc	carry
+	movwf	fcounter, ACCESS
 	retfie	FAST		; fast return from interrupt
-carry
-	movlw	0x0
-	addwfc	loopsh, 1
-	clrf	loopsl
+timer
+	call	calc
+	clrf	fcounter
+	clrf	frequencyl
+	clrf	frequencyh
 	retfie	FAST
 	
 
@@ -23,10 +26,13 @@ DAC	code
 Timer_Setup
 	clrf	TRISD		; Set PORTD as all outputs
 	clrf	LATD		; Clear PORTD outputs
-	movlw	b'10000111'	; Set timer0 to 16-bit, Fosc/4/256
+	movlw	b'10000110'	; Set timer0 to 16-bit, Fosc/4/256
 	movwf	T0CON		; = 62.5KHz clock rate, approx 1sec rollover
 	bsf	INTCON,TMR0IE	; Enable timer0 interrupt
 	bsf	INTCON,GIE	; Enable all interrupts
 	return
 	
+CM_Setup
+	movlw	b'10001100'
+	movwf	CM1CON
 	end
